7f1fcac5a222862a3cf9dbc110f23055d02988dc
Daniel San <daniel.sam123@hotmail.com>
2015-12-28 18:04:15 -0300

    Now databinder works!
    Signed-off-by: Daniel San <daniel.sam123@hotmail.com>

--- app/src/main/java/br/com/danielsan/dscontacts/activities/MainActivity.java
+++ app/src/main/java/br/com/danielsan/dscontacts/activities/MainActivity.java
@@ -1,17 +1,36 @@
 package br.com.danielsan.dscontacts.activities;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.view.MenuItem;
 
+import br.com.danielsan.dscontacts.R;
 import br.com.ilhasoft.support.view.BaseActivity;
+
+import br.com.danielsan.dscontacts.databinding.ActivityMainBinding;
 
 /**
  * Created by daniel on 28/12/15.
  */
 public class MainActivity extends BaseActivity {
 
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.nvgtVw.setNavigationItemSelectedListener(onNavigationItemSelected);
     }
+
+    private final NavigationView.OnNavigationItemSelectedListener onNavigationItemSelected = new NavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            item.setChecked(true);
+            binding.drwLyt.closeDrawers();
+            return false;
+        }
+    };
 
 }
