e7ff09d8a1e87985d02173dc7c35b375571f4500
Daniel San <daniel.sam123@hotmail.com>
2015-12-28 17:00:28 -0300

    Initial configuration for this project.
    Signed-off-by: Daniel San <daniel.sam123@hotmail.com>

---
+++ app/src/main/java/br/com/danielsan/dscontacts/activities/MainActivity.java
@@ -0,0 +1,17 @@
+package br.com.danielsan.dscontacts.activities;
+
+import android.os.Bundle;
+
+import br.com.ilhasoft.support.view.BaseActivity;
+
+/**
+ * Created by daniel on 28/12/15.
+ */
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
