95a29ba12a0381e8c22e4c1ba68e11860c95e39a
Daniel San <daniel.sam123@hotmail.com>
2016-03-12 14:57:50 -0300

    Update android support library
    Signed-off-by: Daniel San <daniel.sam123@hotmail.com>

--- app/src/main/java/br/com/danielsan/dscontacts/managers/fields/edition/GroupFieldEditionManager.java
+++ app/src/main/java/br/com/danielsan/dscontacts/managers/fields/edition/GroupFieldEditionManager.java
@@ -2,20 +2,18 @@ package br.com.danielsan.dscontacts.managers.fields.edition;
 
 import android.os.Bundle;
 import android.os.Parcel;
-import android.view.Gravity;
+import android.support.v7.widget.AppCompatEditText;
 import android.view.LayoutInflater;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import br.com.danielsan.dscontacts.R;
-import br.com.danielsan.dscontacts.databinding.FieldEditionPictureBinding;
 
 /**
  * Created by daniel on 03/01/16.
  */
 public class GroupFieldEditionManager extends FieldEditionManager {
 
-    private EditText editText;
+    private AppCompatEditText editText;
 
     public GroupFieldEditionManager() {
         super(R.drawable.ic_people_gray_24dp, R.string.group);
@@ -27,7 +25,7 @@ public class GroupFieldEditionManager extends FieldEditionManager {
 
     @Override
     public void onCreateSubView(LayoutInflater inflater, LinearLayout fieldContainer, Bundle savedInstanceState) {
-        editText = new EditText(fieldContainer.getContext());
+        editText = new AppCompatEditText(fieldContainer.getContext());
         editText.setHint(R.string.group);
         fieldContainer.addView(editText);
     }
